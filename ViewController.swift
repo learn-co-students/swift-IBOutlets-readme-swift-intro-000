575742c9c61c214334378639f6dc985069738e21
DominationD <dominationdnyc@yahoo.com>
2017-05-20 13:06:28 -0400

    1st App

--- ViewController.swift
+++ ViewController.swift
@@ -10,15 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textDisplay: UILabel!
+    
+    @IBAction func helloWorld(_ sender: AnyObject) {
+        print("Pressed Hello World button")
+        textDisplay.text = "Hello, world!"
+    }
+    
+    @IBAction func goodbyeWorld(_ sender: Any) {
+        print("Pressed Goodbye World button")
+        textDisplay.text = "Goodbye, World!"
+    }
+    
+    @IBAction func Unicorn(_ sender: Any) {
+        print("Pressed Unicorn button")
+        textDisplay.text = "🦄"
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        textDisplay.text = "Zzz..."
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
-
+    
    
 
 }
